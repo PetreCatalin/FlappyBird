0d50e627c8a24c2cb78db504230e531a66ed553f
PetreCatalin <catalin_petre2007@yahoo.com>
2017-05-31 20:24:47 +0300

    final version
    final version

--- flappyBird.js
+++ flappyBird.js
@@ -1,10 +1,14 @@
 var bird;
 var pipes = [];
+var numberOfPipesScore = 0;
+var frequnceOfPipes =200;
 
 function setup() {
     createCanvas(window.innerWidth-20,window.innerHeight-20);
+    numberOfPipesScore = 0;
+    pipes.length=0; //cand incepem jocul din nou stergem pipe-urile anterioare
     bird = new Bird();
-    pipes.push(new Pipe());
+    pipes.push(new Pipe()); // un pipe initial
 }
 
 function draw() {
@@ -12,15 +16,29 @@ function draw() {
     bird.update();
     bird.draw();
 
-    for (var i = 0; i<pipes.length;++i) {
+    if (frameCount%frequnceOfPipes==0) {
+        pipes.push(new Pipe());  // la un numar de frame-uri adaugam un pipe nou
+        if (frequnceOfPipes>=50) frequnceOfPipes-=2; // facem pipe-urile din ce in ce mai dese
+        if (frequnceOfPipes<50) frequnceOfPipes+=50;
+    }
+
+    for (var i = pipes.length-1; i>=0 ;--i) {
         pipes[i].draw();
         pipes[i].update();
+
+        if(pipes[i].hits(bird)) {      //pasarea a lovit un pipe
+            alert('Scor obtinut: ' +numberOfPipesScore +". Incercati din nou!");
+            location.reload();
+        }
+
+        if (pipes[i].x < 0) { //daca pipe-ul a iesit din ecran inseamna ca am trecut de el cu succes si il stergem
+            pipes.splice(i,1);
+            numberOfPipesScore++;
+        }
     }
 }
 
 function keyPressed() {
-    if (key===' ') //spacebar
+    if (key===' ') //spacebar -> pasarea urca
         bird.up();
 }
-
-
--- pipe.js
+++ pipe.js
@@ -1,22 +1,49 @@
-function Pipe () {
-    this.top = Math.random()*400;
-    this.bottom = Math.random()*400;
-    this.x = window.innerWidth - 50;
-    this.speed=3; //viteza de deplasare a dreptunghiurilor
+var initialSpeed = 4; //viteza initiala de deplasare a pipe-urilor
+var distance1=200;
+var distance2=400;
 
-    while (this.top+this.bottom<550) {  // ne asiguram ca gaura pentru pasare este suficient de mica
+function Pipe () {
+    this.top = Math.random()*distance1; //generam lungimile pipe-urilor
+    this.bottom = Math.random()*distance2;
+
+    var aux=distance1; //alteram distanta sus jos pe fiecare nou pipe fata de precedentul
+    distance1=distance2;
+    distance2=aux;
+
+    this.x = window.innerWidth - 50;
+    this.speed=initialSpeed;
+
+    while (this.top+this.bottom<580) {  // ne asiguram ca gaura pentru pasare este suficient de mica
         this.top+=10;
         this.bottom+=10;
     }
 
     this.draw = function () {
-        fill(255,255,255);
+        fill(0,255,0);
+
         rect(this.x,0,40,this.top);
         rect(this.x,window.innerHeight-this.bottom-20,40,this.bottom);
     };
 
     this.update = function () {
         this.x-=this.speed;
-    }
-}
 
+        if (frameCount%800==0) {  // viteza de deplasare a pipe-urilor devine mai mare cand avansam in joc
+            if (initialSpeed<=25) initialSpeed+=Math.random()*2/3;
+        }
+    };
+
+    this.hits = function (bird) {
+        if (bird.positionY < this.top+10 || bird.positionY > window.innerHeight-40-this.bottom) {   // se intersecteaza cu un pipe pe verticala
+            //alert(bird.positionX + ' '+this.x);
+            if (bird.positionX >= this.x - 20 && bird.positionX <= this.x + 20)  //verificam sa fie in dreptul pipe-ului respectiv
+            {
+               // console.log('lovit');
+                return true;
+            }
+        }
+        //console.log('nelovit');
+        return false;
+    };
+
+}
